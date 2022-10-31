ffd78f469e4606f28642d2d4a7ca7fadb87fdf10
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-31 17:06:15 +0100

    non working refactor

--- src/main/java/no/academy/tictac/Game.java
+++ src/main/java/no/academy/tictac/Game.java
@@ -14,15 +14,33 @@ public class Game {
     int counter =0;
     public int determineWinner(int[][] board) {
         counter++;
-
+        // check rows
         for (int i = 0; i < board.length; i++) {
-            if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] == board[i][3] && board[i][3]==board[i][4]))
-                return board[i][2];
+            int value = board[i][0]; //set the value to the first field being evaluated
+            int correctCount = 0;
+            //check if values of all fields in a row are the same
+            for (int j = 0; j < board.length; j++) {
+                if ((board[i][j] == value)){ //check if values on row have same value
+                    correctCount++;
+                }
+            }
+            if (correctCount==5) return value;
         }
-        for (int j = 0; j < board.length ; j++) {
+        // check columns
+        for (int l = 0; l < board.length; l++) {
+            int value = board[0][l];
+            int correctCount = 0;
+            for (int j = 0; j < board.length; j++) {
+                if ((board[j][l] == value)){
+                    correctCount++;
+                }
+            }
+            if (correctCount==5) return value;
+        }
+        /*for (int j = 0; j < board.length ; j++) {
             if ((board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[2][j] == board[3][j] && board[3][j] == board[4][j]))
                 return board[2][j];
-        }
+        }*/
             if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == board[3][3] && board[3][3] == board[4][4]
                 || board[0][4] == board[1][3] && board[1][3] ==board[2][2] && board[3][1] ==board[4][0])){
                 return board[2][2];
